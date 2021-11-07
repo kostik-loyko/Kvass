9febd79f3e63309fb5864ef759d3f7d42992db28
kostik loyko <kostik.loyko@mail.ru>
2021-11-07 22:04:30 +0300

    select js part 1

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector(".down-list__select").addEventListener("click", function () {
+   document.querySelector(".down-list__items").classList.toggle("down-list__items--active");
+})
