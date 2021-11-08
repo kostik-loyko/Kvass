9445a0c247e3e8cb1903af8fd27fda0f18062f8c
kostik loyko <kostik.loyko@mail.ru>
2021-11-08 23:50:29 +0300

    select js part 2

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-document.querySelector(".down-list__select").addEventListener("click", function () {
+// клик по кнопке открь/закрыть селект
+
+document.querySelector(".down-list__select").addEventListener("click", function (event) {
+   event.stopPropagation();
    document.querySelector(".down-list__items").classList.toggle("down-list__items--active");
+   document.querySelector(".down-list__select").classList.toggle("down-list__select--active");
+})
+
+// выбор элемента из спика/ запомнить выбранное значение/ закрыть селект
+
+document.querySelectorAll(".down-list__item").forEach(function (listItem) {
+   listItem.addEventListener("click", function (event) {
+      event.stopPropagation();
+      document.querySelector(".down-list__select").textContent = this.textContent;
+      document.querySelector(".down-list__select").focus();
+      document.querySelector(".down-list__select").classList.remove("down-list__select--active");
+      document.querySelector(".down-list__items").classList.remove("down-list__items--active");
+      document.querySelector(".down-list__input").value = this.dataset.value;
+   })
+})
+
+// клик снаружи селекта - закрытие селекта
+
+document.addEventListener("click", function () {
+   console.log("click");
+   document.querySelector(".down-list__items").classList.remove("down-list__items--active");
 })
